0f9236a65ca665f51a5a9f18180b2494238de34c
Riadh HAJ AMOR <rhadjamor@gmail.com>
2014-09-04 17:16:00 +0200

    added support for the traffic profile

--- packetcable-provider/src/main/java/org/opendaylight/controller/packetcable/provider/OpenDaylightPacketCableProviderService.java
+++ packetcable-provider/src/main/java/org/opendaylight/controller/packetcable/provider/OpenDaylightPacketCableProviderService.java
@@ -1,5 +1,16 @@
 package org.opendaylight.controller.packetcable.provider;
 
-public interface OpenDaylightPacketCableProviderService {
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.TrafficProfileBestEffortAttributes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.add.flow.input.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.flowspec._case.Flowspec;
+import org.pcmm.gates.ITrafficProfile;
+import org.pcmm.gates.impl.Classifier;
+
+public interface OpenDaylightPacketCableProviderService { 
+	
+	public ITrafficProfile	buildTrafficProfile(TrafficProfileBestEffortAttributes bestEffort);
+
+	public ITrafficProfile	buildTrafficProfile(Flowspec bestEffort);
+	
+	public Classifier getClassifier();
 
 }
--- packetcable-provider/src/main/java/org/opendaylight/controller/packetcable/provider/OpendaylightPacketcableProvider.java
+++ packetcable-provider/src/main/java/org/opendaylight/controller/packetcable/provider/OpendaylightPacketcableProvider.java
@@ -17,6 +17,9 @@ import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderCo
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RoutedRpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
 import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowOutput;
@@ -27,12 +30,13 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.Upda
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.flow.update.OriginalFlow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.flow.update.UpdatedFlow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.TrafficProfileBestEffortAttributes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.add.flow.input.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.BestEffortCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.add.flow.input.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.FlowspecCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.add.flow.input.instructions.instruction.instruction.apply.actions._case.apply.actions.action.action.flowspec._case.Flowspec;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.traffic.profile.rev140808.traffic.profile.best.effort.attributes.BeAuthorizedEnvelope;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ApplyActionsCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.node.cmts.rev140120.CmtsCapableNode;
@@ -44,23 +48,27 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.packetcable.match.types.rev
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packetcable.match.types.rev140120.UdpMatchRangesRpcUpdateFlowOriginal;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packetcable.match.types.rev140120.UdpMatchRangesRpcUpdateFlowUpdated;
 import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.concepts.CompositeObjectRegistration.CompositeObjectRegistrationBuilder;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.pcmm.PCMMGlobalConfig;
+import org.pcmm.gates.ITrafficProfile;
+import org.pcmm.gates.impl.BestEffortService;
+import org.pcmm.gates.impl.BestEffortService.BEEnvelop;
+import org.pcmm.gates.impl.Classifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Table;
-
-
 @SuppressWarnings("unused")
-public class OpendaylightPacketcableProvider implements
-		DataChangeListener, SalFlowService, OpenDaylightPacketCableProviderService, BindingAwareProvider, AutoCloseable {
+public class OpendaylightPacketcableProvider implements DataChangeListener,
+		SalFlowService, OpenDaylightPacketCableProviderService,
+		BindingAwareProvider, AutoCloseable {
 
-	private static final Logger logger = LoggerFactory.getLogger(OpendaylightPacketcableProvider.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(OpendaylightPacketcableProvider.class);
 	private NotificationProviderService notificationProvider;
 	private DataBroker dataProvider;
 
@@ -69,18 +77,18 @@ public class OpendaylightPacketcableProvider implements
 	// The following holds the Future for the current make toast task.
 	// This is used to cancel the current toast.
 	private final AtomicReference<Future<?>> currentConnectionsTasks = new AtomicReference<>();
-    private ProviderContext providerContext;
-    private NotificationProviderService notificationService;
-    private DataBroker dataBroker;
-    private ListenerRegistration<DataChangeListener> listenerRegistration;
-
+	private ProviderContext providerContext;
+	private NotificationProviderService notificationService;
+	private DataBroker dataBroker;
+	private ListenerRegistration<DataChangeListener> listenerRegistration;
 
 	public OpendaylightPacketcableProvider() {
 		executor = Executors.newCachedThreadPool();
 
 	}
 
-	public void setNotificationProvider(final NotificationProviderService salService) {
+	public void setNotificationProvider(
+			final NotificationProviderService salService) {
 		this.notificationProvider = salService;
 	}
 
@@ -94,23 +102,24 @@ public class OpendaylightPacketcableProvider implements
 	@Override
 	public void close() throws ExecutionException, InterruptedException {
 		executor.shutdown();
-//		if (dataProvider != null) {
-//			for (Iterator<InstanceIdentifier<CmtsInstance>> iter = cmtsInstances.iterator(); iter.hasNext();) {
-//				WriteTransaction tx = dataProvider.newWriteOnlyTransaction();
-//				tx.delete(LogicalDatastoreType.OPERATIONAL, iter.next());
-//				Futures.addCallback(tx.submit(), new FutureCallback<Void>() {
-//					@Override
-//					public void onSuccess(final Void result) {
-//						logger.debug("Delete commit result: " + result);
-//					}
-//
-//					@Override
-//					public void onFailure(final Throwable t) {
-//						logger.error("Delete operation failed", t);
-//					}
-//				});
-//			}
-//		}
+		// if (dataProvider != null) {
+		// for (Iterator<InstanceIdentifier<CmtsInstance>> iter =
+		// cmtsInstances.iterator(); iter.hasNext();) {
+		// WriteTransaction tx = dataProvider.newWriteOnlyTransaction();
+		// tx.delete(LogicalDatastoreType.OPERATIONAL, iter.next());
+		// Futures.addCallback(tx.submit(), new FutureCallback<Void>() {
+		// @Override
+		// public void onSuccess(final Void result) {
+		// logger.debug("Delete commit result: " + result);
+		// }
+		//
+		// @Override
+		// public void onFailure(final Throwable t) {
+		// logger.error("Delete operation failed", t);
+		// }
+		// });
+		// }
+		// }
 	}
 
 	// private CmtsInstance buildCmtsConnection(final String host) {
@@ -129,93 +138,157 @@ public class OpendaylightPacketcableProvider implements
 	 * Implemented from the DataChangeListener interface.
 	 */
 	@Override
-	public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+	public void onDataChanged(
+			final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
 		DataObject dataObject = change.getUpdatedSubtree();
-		logger.debug("OpendaylightPacketcableProvider.onDataChanged() :" + dataObject);
+		logger.debug("OpendaylightPacketcableProvider.onDataChanged() :"
+				+ dataObject);
 	}
 
-    @Override
-    public Future<RpcResult<AddFlowOutput>> addFlow(AddFlowInput input) {
-        // Examples of how to unpack the matches
-        UdpMatchRangesRpcAddFlow updRange = input.getMatch().getAugmentation(UdpMatchRangesRpcAddFlow.class);
-        updRange.getUpdMatchRanges().getUdpDestinationPortStart();
-        TcpMatchRangesRpcAddFlow tcpRange = input.getMatch().getAugmentation(TcpMatchRangesRpcAddFlow.class);
-        tcpRange.getTcpMatchRanges().getTcpDestinationPortBegin();
-        SubscriberIdRpcAddFlow subId = input.getMatch().getAugmentation(SubscriberIdRpcAddFlow.class);
-        subId.getSubscriberId().getIpv6Address();
-        // Examples of how to unpack the actions
-        for( Instruction i : input.getInstructions().getInstruction()) {
-            if(i.getInstruction() instanceof ApplyActionsCase) {
-                ApplyActionsCase aac = (ApplyActionsCase)i.getInstruction();
-                for (Action a : aac.getApplyActions().getAction() ) {
-                    if(a.getAction() instanceof FlowspecCase) {
-                        Flowspec flowSpec = ((FlowspecCase)a.getAction()).getFlowspec();
-                        flowSpec.getFAuthorizedEnvelope();
-                    }
-                }
-            }
-        }
-        return null;
-    }
+	@Override
+	public Future<RpcResult<AddFlowOutput>> addFlow(AddFlowInput input) {
+		// Examples of how to unpack the matches
+		UdpMatchRangesRpcAddFlow updRange = input.getMatch().getAugmentation(
+				UdpMatchRangesRpcAddFlow.class);
+		PortNumber udpDestinationPortStart = updRange.getUpdMatchRanges()
+				.getUdpDestinationPortStart();
+		TcpMatchRangesRpcAddFlow tcpRange = input.getMatch().getAugmentation(
+				TcpMatchRangesRpcAddFlow.class);
+		PortNumber tcpDestinationPortBegin = tcpRange.getTcpMatchRanges()
+				.getTcpDestinationPortBegin();
+		SubscriberIdRpcAddFlow subId = input.getMatch().getAugmentation(
+				SubscriberIdRpcAddFlow.class);
+		Ipv6Address ipv6Address = subId.getSubscriberId().getIpv6Address();
+		Ipv4Address ipv4Address = subId.getSubscriberId().getIpv4Address();
+		// Examples of how to unpack the actions
+		for (Instruction i : input.getInstructions().getInstruction()) {
+			if (i.getInstruction() instanceof ApplyActionsCase) {
+				ApplyActionsCase aac = (ApplyActionsCase) i.getInstruction();
+				for (Action a : aac.getApplyActions().getAction()) {
+					if (a.getAction() instanceof FlowspecCase) {
+						Flowspec flowSpec = ((FlowspecCase) a.getAction())
+								.getFlowspec();
+						flowSpec.getFAuthorizedEnvelope();
+					} else if (a.getAction() instanceof BestEffortCase) {
+						ITrafficProfile tf = buildTrafficProfile(((BestEffortCase) a
+								.getAction()).getBestEffort());
+					}
+				}
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public Future<RpcResult<RemoveFlowOutput>> removeFlow(RemoveFlowInput input) {
-        UdpMatchRangesRpcRemoveFlow updRange = input.getMatch().getAugmentation(UdpMatchRangesRpcRemoveFlow.class);
-        return null;
-    }
+	@Override
+	public ITrafficProfile buildTrafficProfile(TrafficProfileBestEffortAttributes bestEffort) {
+		BestEffortService trafficProfile = new BestEffortService(BestEffortService.DEFAULT_ENVELOP);
+		BEEnvelop authorizedEnvelop = trafficProfile.getAuthorizedEnvelop();
+		BeAuthorizedEnvelope beAuthorizedEnvelope = bestEffort.getBeAuthorizedEnvelope();
+		if(beAuthorizedEnvelope.getTrafficPriority()!=null)
+			authorizedEnvelop.setTrafficPriority(beAuthorizedEnvelope.getTrafficPriority().byteValue());
+		else 
+			authorizedEnvelop.setTrafficPriority(BestEffortService.DEFAULT_TRAFFIC_PRIORITY);
+		if(beAuthorizedEnvelope.getMaximumTrafficBurst()!=null)
+			authorizedEnvelop.setMaximumTrafficBurst(beAuthorizedEnvelope.getMaximumTrafficBurst().intValue());
+		else
+			authorizedEnvelop.setMaximumTrafficBurst(BestEffortService.DEFAULT_MAX_TRAFFIC_BURST);
+		if(beAuthorizedEnvelope.getRequestTransmissionPolicy()!=null)
+			authorizedEnvelop.setRequestTransmissionPolicy(beAuthorizedEnvelope.getRequestTransmissionPolicy().intValue());
+		if(beAuthorizedEnvelope.getMaximumSustainedTrafficRate()!=null)
+			authorizedEnvelop.setMaximumSustainedTrafficRate(beAuthorizedEnvelope.getMaximumSustainedTrafficRate().intValue());
 
-    @Override
-    public Future<RpcResult<UpdateFlowOutput>> updateFlow(UpdateFlowInput input) {
-        OriginalFlow foo = input.getOriginalFlow();
-        UdpMatchRangesRpcUpdateFlowOriginal bar = foo.getMatch().getAugmentation(UdpMatchRangesRpcUpdateFlowOriginal.class);
-        UpdatedFlow updated = input.getUpdatedFlow();
-        UdpMatchRangesRpcUpdateFlowUpdated updatedRange = updated.getMatch().getAugmentation(UdpMatchRangesRpcUpdateFlowUpdated.class);
+		BEEnvelop reservedEnvelop = trafficProfile.getReservedEnvelop();
+		reservedEnvelop.setTrafficPriority(reservedEnvelop.getTrafficPriority());
+		reservedEnvelop.setMaximumTrafficBurst(BestEffortService.DEFAULT_MAX_TRAFFIC_BURST);
+		reservedEnvelop.setMaximumTrafficBurst(BestEffortService.DEFAULT_MAX_TRAFFIC_BURST);
+		reservedEnvelop.setRequestTransmissionPolicy(PCMMGlobalConfig.BETransmissionPolicy);
+		reservedEnvelop.setMaximumSustainedTrafficRate(PCMMGlobalConfig.DefaultLowBestEffortTrafficRate);
 
-        return null;
-    }
+		BEEnvelop committedEnvelop =  trafficProfile.getCommittedEnvelop();
+		committedEnvelop.setTrafficPriority(BestEffortService.DEFAULT_TRAFFIC_PRIORITY);
+		committedEnvelop.setMaximumTrafficBurst(BestEffortService.DEFAULT_MAX_TRAFFIC_BURST);
+		committedEnvelop.setRequestTransmissionPolicy(PCMMGlobalConfig.BETransmissionPolicy);
+		committedEnvelop.setMaximumSustainedTrafficRate(PCMMGlobalConfig.DefaultLowBestEffortTrafficRate);
+		return null;
+	}
 
-    @Override
-    public Collection<? extends RpcService> getImplementations() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public ITrafficProfile buildTrafficProfile(Flowspec bestEffort) {
+		return null;
+	}
 
-    @Override
-    public Collection<? extends ProviderFunctionality> getFunctionality() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Classifier getClassifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void onSessionInitiated(ProviderContext session) {
-        providerContext = session;
-        notificationService = session.getSALService(NotificationProviderService.class);
-        dataBroker = session.getSALService(DataBroker.class);
-        InstanceIdentifier<CmtsNode> listenTo = InstanceIdentifier.create(Nodes.class)
-                                                                                .child(Node.class)
-                                                                                .augmentation(CmtsCapableNode.class)
-                                                                                .child(CmtsNode.class);
-        listenerRegistration = dataBroker.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
-                listenTo, this, DataChangeScope.BASE);
-    }
+	@Override
+	public Future<RpcResult<RemoveFlowOutput>> removeFlow(RemoveFlowInput input) {
+		UdpMatchRangesRpcRemoveFlow updRange = input.getMatch()
+				.getAugmentation(UdpMatchRangesRpcRemoveFlow.class);
+		return null;
+	}
 
-    @Override
-    public void onSessionInitialized(ConsumerContext session) {
-        // Noop
+	@Override
+	public Future<RpcResult<UpdateFlowOutput>> updateFlow(UpdateFlowInput input) {
+		OriginalFlow foo = input.getOriginalFlow();
+		UdpMatchRangesRpcUpdateFlowOriginal bar = foo.getMatch()
+				.getAugmentation(UdpMatchRangesRpcUpdateFlowOriginal.class);
+		UpdatedFlow updated = input.getUpdatedFlow();
+		UdpMatchRangesRpcUpdateFlowUpdated updatedRange = updated.getMatch()
+				.getAugmentation(UdpMatchRangesRpcUpdateFlowUpdated.class);
 
-    }
+		return null;
+	}
 
-    public void onSessionAdded(/* Whatever you need per CmtsConnection */) {
-        CompositeObjectRegistrationBuilder<OpendaylightPacketcableProvider> builder = CompositeObjectRegistration
-                .<OpendaylightPacketcableProvider> builderFor(this);
-        /*
-         * You will need a routedRpc registration per Cmts... I'm not doing the accounting of storing them here, but you will
-         * need to so you can close them when your provider is closed
-         */
-        RoutedRpcRegistration<SalFlowService> registration = providerContext.addRoutedRpcImplementation(SalFlowService.class, this);
-        /* You will need to get your identifier somewhere... this is your nodeId.  I would recommend adoption a convention like
-         * "cmts:<ipaddress>" for CmtsCapableNodes
-        registration.registerPath(NodeContext.class, getIdentifier());
-        */
-    }
+	@Override
+	public Collection<? extends RpcService> getImplementations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<? extends ProviderFunctionality> getFunctionality() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onSessionInitiated(ProviderContext session) {
+		providerContext = session;
+		notificationService = session
+				.getSALService(NotificationProviderService.class);
+		dataBroker = session.getSALService(DataBroker.class);
+		InstanceIdentifier<CmtsNode> listenTo = InstanceIdentifier
+				.create(Nodes.class).child(Node.class)
+				.augmentation(CmtsCapableNode.class).child(CmtsNode.class);
+		listenerRegistration = dataBroker.registerDataChangeListener(
+				LogicalDatastoreType.CONFIGURATION, listenTo, this,
+				DataChangeScope.BASE);
+	}
+
+	@Override
+	public void onSessionInitialized(ConsumerContext session) {
+		// Noop
+
+	}
+
+	public void onSessionAdded(/* Whatever you need per CmtsConnection */) {
+		CompositeObjectRegistrationBuilder<OpendaylightPacketcableProvider> builder = CompositeObjectRegistration
+				.<OpendaylightPacketcableProvider> builderFor(this);
+		/*
+		 * You will need a routedRpc registration per Cmts... I'm not doing the
+		 * accounting of storing them here, but you will need to so you can
+		 * close them when your provider is closed
+		 */
+		RoutedRpcRegistration<SalFlowService> registration = providerContext
+				.addRoutedRpcImplementation(SalFlowService.class, this);
+		/*
+		 * You will need to get your identifier somewhere... this is your
+		 * nodeId. I would recommend adoption a convention like
+		 * "cmts:<ipaddress>" for CmtsCapableNodes
+		 * registration.registerPath(NodeContext.class, getIdentifier());
+		 */
+	}
 }
