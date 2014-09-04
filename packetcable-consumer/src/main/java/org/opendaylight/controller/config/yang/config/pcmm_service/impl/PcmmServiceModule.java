47aa68caba31cfc9b439f61dedaa2dd2152f77cd
Riadh HAJ AMOR <rhadjamor@gmail.com>
2014-09-04 02:22:19 +0200

    changes to make it compile

--- packetcable-consumer/src/main/java/org/opendaylight/controller/config/yang/config/pcmm_service/impl/PcmmServiceModule.java
+++ packetcable-consumer/src/main/java/org/opendaylight/controller/config/yang/config/pcmm_service/impl/PcmmServiceModule.java
@@ -2,7 +2,7 @@ package org.opendaylight.controller.config.yang.config.pcmm_service.impl;
 
 import org.opendaylight.controller.org.pcmm.api.PcmmService;
 import org.opendaylight.controller.org.pcmm.impl.PcmmServiceImpl;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packetcable.service.rev140120.PacketcableServiceService;
+//import org.opendaylight.yang.gen.v1.urn.opendaylight.packetcable.service.rev140120.PacketcableServiceService;
 import org.opendaylight.yangtools.concepts.Registration;
 
 public class PcmmServiceModule
@@ -23,30 +23,31 @@ public class PcmmServiceModule
 
 	@Override
 	public java.lang.AutoCloseable createInstance() {
-		PacketcableServiceService packetcableServiceService = getRpcRegistryDependency().getRpcService(PacketcableServiceService.class);
-		final PcmmService pcmmService = new PcmmServiceImpl(packetcableServiceService);
-		final Registration pcmmListenerReg = getNotificationServiceDependency().registerNotificationListener(pcmmService);
-		final PcmmServiceRuntimeRegistration runtimeReg = getRootRuntimeBeanRegistratorWrapper().register(pcmmService);
-		return new AutoCloseablePcmmService(packetcableServiceService, pcmmListenerReg, runtimeReg);
+//		PacketcableServiceService packetcableServiceService = getRpcRegistryDependency().getRpcService(PacketcableServiceService.class);
+//		final PcmmService pcmmService = new PcmmServiceImpl(packetcableServiceService);
+//		final Registration pcmmListenerReg = getNotificationServiceDependency().registerNotificationListener(pcmmService);
+//		final PcmmServiceRuntimeRegistration runtimeReg = getRootRuntimeBeanRegistratorWrapper().register(pcmmService);
+//		return new AutoCloseablePcmmService(packetcableServiceService, pcmmListenerReg, runtimeReg);
+		return null;
 	}
 
-	class AutoCloseablePcmmService extends PcmmServiceImpl implements
-			AutoCloseable {
-		
-		private PcmmServiceRuntimeRegistration runtimeReg;
-		private Registration pcmmListenerReg;
-
-		public AutoCloseablePcmmService(PacketcableServiceService packetcableServiceService, Registration pcmmListenerReg, PcmmServiceRuntimeRegistration runtimeReg) {
-			super(packetcableServiceService);
-			this.runtimeReg = runtimeReg;
-			this.pcmmListenerReg = pcmmListenerReg;
-		}
-
-		@Override
-		public void close() throws Exception {
-			pcmmListenerReg.close();
-			runtimeReg.close();
-		}
-
-	}
+//	class AutoCloseablePcmmService extends PcmmServiceImpl implements
+//			AutoCloseable {
+//		
+//		private PcmmServiceRuntimeRegistration runtimeReg;
+//		private Registration pcmmListenerReg;
+//
+//		public AutoCloseablePcmmService(PacketcableServiceService packetcableServiceService, Registration pcmmListenerReg, PcmmServiceRuntimeRegistration runtimeReg) {
+//			super(packetcableServiceService);
+//			this.runtimeReg = runtimeReg;
+//			this.pcmmListenerReg = pcmmListenerReg;
+//		}
+//
+//		@Override
+//		public void close() throws Exception {
+//			pcmmListenerReg.close();
+//			runtimeReg.close();
+//		}
+//
+//	}
 }
